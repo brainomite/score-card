c012b090a1762d386bf6238e266768b7045e3b7b
Aaron Young <hi@aaronyoung.dev>
2024-09-04 18:35:28 -0400

    feat: change hello world

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,14 +10,10 @@ if (!process.env.FRONTEND_URL) {
     origin: process.env.FRONTEND_URL,
   };
   app.use(cors(corsOptions));
-  // console.log(
-  //   `CORS is enabled only for ${process.env.FRONTEND_URL}`,
-  //   JSON.stringify(corsOptions)
-  // );
 }
 
 app.get("/api", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! From the backend!");
 });
 
 const { PORT = 3000 } = process.env;
