92b602f9488b496919fa3d6eb9c3c5dba106ee8c
Aaron Young <hi@aaronyoung.dev>
2024-09-07 12:20:58 -0400

    bug: fix access to subroutes4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,8 +16,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
+        main: "./index.html",
+        404: "./public/404.html",
       },
     },
   },
