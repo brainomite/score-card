b697b8bfde10ab6a42a1aed4110ab45f42e4b1f8
Aaron Young <hi@aaronyoung.dev>
2024-09-07 12:14:57 -0400

    bug: fix access to subroutes3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,12 @@ export default defineConfig({
     cors: false,
   },
   base: "/score-card-app/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 });
