0d17eed43be0ae7cc783624f533d0962a9cb294b
Aaron Young <hi@aaronyoung.dev>
2024-09-23 16:16:59 -0400

    switch from <BrowserRouter> to createBrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SetUpCard from "./pages/SetUpCard";
 import ScoreSheet from "./pages/ScoreSheet";
 import { Toaster } from "react-hot-toast";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SetUpCard />,
+  },
+  { path: "/sheet/:id/", element: <ScoreSheet /> },
+]);
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<SetUpCard />} />
-        {/* <Route path="/sheet1/" element={<ScoreSheet />} /> */}
-        {/* // create a route to "sheet/:id" that renders the ScoreSheet component */}
-        <Route path="/sheet/:id/" element={<ScoreSheet />} />
-      </Routes>
+      <RouterProvider router={router} />
       <Toaster />
     </>
   );
