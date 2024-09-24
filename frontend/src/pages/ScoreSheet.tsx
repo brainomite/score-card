da6acdba4dfa5474a6504881e90343d5fa311786
Aaron Young <hi@aaronyoung.dev>
2024-09-24 10:29:29 -0400

    add speed dial

--- frontend/src/pages/ScoreSheet.tsx
+++ frontend/src/pages/ScoreSheet.tsx
@@ -6,12 +6,23 @@ import ResultList from "../components/ResultList";
 import { categoryType } from "../types";
 import { FC, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import Icon from "@mui/material/Icon";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
 
 export const Component: FC = () => {
   const [scoreData, setScoreData] = useState<categoryType>(
     useLoaderData() as categoryType
   );
+  const [speedDialOpen, setSpeedDialOpen] = useState(false);
+  const handleOpen = () => setSpeedDialOpen(true);
+  const handleClose = () => setSpeedDialOpen(false);
   const { id } = useParams() as { id: string };
+  const speedDialActions = [
+    { icon: <Icon>share</Icon>, name: "Share", action: handleClose },
+    { icon: <Icon>note_add</Icon>, name: "New", action: handleClose },
+    { icon: <Icon>lock_reset</Icon>, name: "Reset", action: handleClose },
+  ];
   const updateScoreForCategory =
     (category: string) => (player: string) => async (score: number) => {
       const res = await fetch(`/api/score-card/${id}/`, {
@@ -56,6 +67,24 @@ export const Component: FC = () => {
       >
         {listScoreCategories()}
       </List>
+      <SpeedDial
+        ariaLabel={"Speed Dial"}
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        icon={<Icon>add</Icon>}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={speedDialOpen}
+      >
+        {speedDialActions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={action.action}
+            tooltipOpen
+          />
+        ))}
+      </SpeedDial>
     </Box>
   );
 };
