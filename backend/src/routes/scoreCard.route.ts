a6c47018451c096b8f21c107a2cb6cb4a8b4ea03
Aaron Young <hi@aaronyoung.dev>
2024-09-25 10:32:06 -0400

    refactor

--- backend/src/routes/scoreCard.route.ts
+++ backend/src/routes/scoreCard.route.ts
@@ -11,9 +11,11 @@ const SCORE_CARD_PATH_ID = `${SCORE_CARD_PATH}/:id`;
 const router = Router();
 
 router.get(`${SCORE_CARD_PATH_ID}/subscribe`, subscribeToCard);
-router.get(SCORE_CARD_PATH_ID, getScoreCard);
+router
+  .route(SCORE_CARD_PATH_ID)
+  .get(getScoreCard)
+  .delete(deleteScoreCard)
+  .patch(patchScoreCard);
 router.put(SCORE_CARD_PATH, createScoreCard);
-router.delete(SCORE_CARD_PATH_ID, deleteScoreCard);
-router.patch(SCORE_CARD_PATH_ID, patchScoreCard);
 
 export default router;
